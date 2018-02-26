35dfa887430b63017e5de4f40b1fd1f68d7093a8
XiaobingYue <yuexiaobing_haut@163.com>
2018-02-26 09:49:28 +0800

    dd

--- src/main/java/com/yxb/common/controller/DownloadController.java
+++ src/main/java/com/yxb/common/controller/DownloadController.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.yxb.dispacher.listener.ServerStartUpListener;
-
 @Controller
 @RequestMapping("/downloadController")
 public class DownloadController {
